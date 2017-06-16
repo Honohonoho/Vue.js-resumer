64de5f291e6f6ed301b0b06a17bb55d3610e337b
Honohonoho <751718620@qq.com>
2017-06-16 15:52:39 +0800

    显示用户名和登出

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -3,12 +3,17 @@
     <div class="wrapper">
       <span class="logo">Resume</span>
       <div class="actions">
-        <span>{{user}}</span>
-        <a class="button primary" href="#" @click.prevent="signUpDialogVisible = true">注册</a>
-        <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
-          <SignUpForm @success="login($event)"/>
-        </MyDialog>
-        <a class="button" href="#">登录</a>
+        <div v-if="logined" class="userActions">
+          <span>你好,{{user.username}}</span>
+          <a class="button" href="#">登出</a>
+        </div>
+        <div v-else class="userActions">
+          <a class="button primary" href="#" @click.prevent="signUpDialogVisible = true">注册</a>
+          <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
+            <SignUpForm @success="login($event)"/>
+          </MyDialog>
+          <a class="button" href="#">登录</a>
+        </div>
         <button class="button primary">保存</button>
         <button class="button">预览</button>
       </div>
@@ -30,6 +35,9 @@ export default {
   computed: {
     user(){
       return this.$store.state.user
+    },
+    logined(){
+      return this.user.id
     }
   },
   components: {
@@ -84,7 +92,10 @@ export default {
       color: #fff;
     }
   }
-  .action > a{
-
+  .action{
+    display: flex;
+    .userActions{
+      margin-right: 3em;
+    }
   }
 </style>
