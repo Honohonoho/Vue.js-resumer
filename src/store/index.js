1c5f1c81f7c9e338b24561a23d7dcb1efaf3eee4
Honohonoho <751718620@qq.com>
2017-06-16 22:57:08 +0800

    删除所有假数据

--- src/store/index.js
+++ src/store/index.js
@@ -20,36 +20,30 @@ export default new Vuex.Store({
           {'field': 'contacts',icon: 'phone'},
         ],
         profile: {
-          name: '李某',
-          city: '西安',
-          title: '前端新人',
-          birthday: '1993-01-01'
+          name: '',
+          city: '',
+          title: '',
+          birthday: ''
         },
         'workHistory': [
-          {company: 'B公司', content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
-            我的主要工作如下:
-            1. 完成既定产品需求。
-            2. 修复 bug。`},
-          {company: 'A公司', content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
-            我的主要工作如下:
-            1. 完成既定产品需求。
-            2. 修复 bug。`},
+          {
+            company: '', content: ``
+          },
+          {
+            company: '', content: ``
+          },
         ],
         education: [
-          {school: '某大学',content: '硕士'},
-          {school: '某大学',content: '本科'},
+          {school: '',content: ''},
         ],
         projects: [
-          {name: 'project A',content: '文字'},
-          {name: 'project B',content: '文字'},
+          {name: '',content: ''},
         ],
         awards: [
-          {name: '人品爆炸',content: '连续十次获得「再来一瓶」奖励'},
-          {name: '奖学金',content: '一等奖学金'},
+          {name: '',content: ''},
         ],
         contacts: [
-          {contact: 'phone',content: '13812345678'},
-          {contact: 'qq', content: '12345678'},
+          {contact: '',content: ''},
         ],
       }
     },
