d6859d10b2d0d9d26055d9b0eb7dc11aa258ca06
Honohonoho <751718620@qq.com>
2017-06-21 15:14:45 +0800

    更新删除按钮的样式

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -17,7 +17,7 @@
         <div v-if="item.type === 'array'">
           <h2>{{$t(`resume.${item.field}._`)}}</h2>
           <div class="subitem" v-for="(subitem, i) in resume[item.field]">
-            <button class="button remove">删除</button>
+            <button class="button remove small">删除</button>
             <div class="resumeField" v-for="(value,key) in subitem">
               <label>{{$t(`resume.${item.field}.${key}`)}}</label>
               <input type="text" :value="value" @input="changeResumeField(`${item.field}.${i}.${key}`, $event.target.value)">
@@ -129,12 +129,12 @@ export default {
     border-top: 2px solid #ddd;
     margin: 24px 0;
   }
-  .submit{
+  .subitem{
     position: relative;
     .button.remove{
       position: absolute;
       right: 0;
-      top: 0;
+      top: -5px;
     }
   }
 </style>
