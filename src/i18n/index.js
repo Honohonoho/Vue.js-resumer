6a9270649f8a6c96dd078b95dd1d7945a4ca05dd
Honohonoho <751718620@qq.com>
2017-06-21 11:51:47 +0800

    重构 i18n，将所有文字放到一个文件里

---
+++ src/i18n/index.js
@@ -0,0 +1,47 @@
+export default {
+  en: {
+    resume: {
+      profile: {
+        name: 'Name',
+        city: 'City',
+        title: 'Jpb Titlte',
+        birthday: 'Birthday'
+      }
+    }
+  },
+  zh_CN: {
+    resume: {
+      profile: {
+        name: '姓名',
+        city: '城市',
+        title: '职位',
+        birthday: '生日'
+      },
+      workHistory: {
+        _: '教育经历',
+        school: '毕业院校',
+        details: '详情'
+      },
+      education: {
+        _: '工作经历',
+        company: '工作单位',
+        details: '详情'
+      },
+      projects: {
+        _: '项目经历',
+        name: '项目名称',
+        details: '项目介绍'
+      },
+      awards: {
+        _: '获奖经历',
+        name: '获奖项目',
+        details: '详情'
+      },
+      contacts: {
+        _: '联系方式',
+        contact: '途径',
+        content: '内容'
+      }
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -4,55 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueI18n from 'vue-i18n'
+import i18n from './i18n'
 
-var locales = {
-  en: {
-    resume: {
-      profile: {
-        name: 'Name',
-        city: 'City',
-        title: 'Jpb Titlte',
-        birthday: 'Birthday'
-      }
-    }
-  },
-  zh_CN: {
-    resume: {
-      profile: {
-        name: '姓名',
-        city: '城市',
-        title: '职位',
-        birthday: '生日'
-      },
-      workHistory: {
-        _: '教育经历',
-        school: '毕业院校',
-        details: '详情'
-      },
-      education: {
-        _: '工作经历',
-        company: '工作单位',
-        details: '详情'
-      },
-      projects: {
-        _: '项目经历',
-        name: '项目名称',
-        details: '项目介绍'
-      },
-      awards: {
-        _: '获奖经历',
-        name: '获奖项目',
-        details: '详情'
-      },
-      contacts: {
-        _: '联系方式',
-        contact: '途径',
-        content: '内容'
-      }
-    }
-  }
-}
-
+var locales = i18n
 Vue.use(VueI18n)
 Vue.config.lang = 'zh_ZN'
 
